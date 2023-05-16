39797f685dd29b3e61b5a8f902d2ea4cc06785aa
Alejandro Toro Serna <altose87@gmail.com>
2023-05-16 09:09:43 -0500

    Use transaction_type instead of type

--- spec/models/user_category_spec.rb
+++ spec/models/user_category_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: user_categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  user_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe UserCategory, type: :model do
